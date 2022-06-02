5fc9e9916b18a87ef5a8a20c7a1b76ca3e56002f
kira-0521 <kiratanaka0521@gmail.com>
2022-06-02 21:53:58 +0900

    [ feature ] Article & User types

---
+++ src/@types/article.d.ts
@@ -0,0 +1,12 @@
+export interface Article {
+  id: string
+  imageUrl: string
+  title: string
+  userId: string
+  content: string
+  contentCount: number
+  createdAt: string // Date
+  updatedAt: string // Date
+  publishedAt: string // Date
+  revisedAt: string // Date
+}
---
+++ src/@types/user.d.ts
@@ -0,0 +1,13 @@
+export interface User {
+  id: string
+  name: string
+  imageUrl: string
+  description?: string
+  twitterUrl?: string
+  facebookUrl?: string
+  firebaseId: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+}
