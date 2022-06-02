4215d426735a50d4f486e1c2755c66c002374d9a
kira-0521 <kiratanaka0521@gmail.com>
2022-06-02 18:08:01 +0900

    [ feature ] axios to instance

---
+++ src/scripts/lib/axios.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+import { MICROCMS_BASE_URL, X_MICROCMS_API_KEY } from '../utils/const'
+
+console.log('baseURL ===> ', MICROCMS_BASE_URL)
+
+const apiInstance = axios.create({
+  headers: {
+    'X-MICROCMS-API-KEY': X_MICROCMS_API_KEY,
+    'Content-Type': 'application/json',
+  },
+  baseURL: MICROCMS_BASE_URL,
+})
+
+export default apiInstance
