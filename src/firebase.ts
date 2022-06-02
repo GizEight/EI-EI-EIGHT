9098379526e611860736f9d22d0b39454bdbd2bf
kira-0521 <kiratanaka0521@gmail.com>
2022-06-02 17:28:21 +0900

    [ feature ] firebase setup

---
+++ src/firebase.ts
@@ -0,0 +1,26 @@
+import { getAnalytics } from 'firebase/analytics'
+import { initializeApp } from 'firebase/app'
+
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} from './scripts/utils/const'
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
+}
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig)
+export const analytics = getAnalytics(firebaseApp)
