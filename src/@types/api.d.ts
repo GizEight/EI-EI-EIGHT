ffe0212b3e82a99aa1284217cf61dc4810c813bf
kira-0521 <kiratanaka0521@gmail.com>
2022-06-03 18:11:20 +0900

    [ add ] 投稿・作成・更新用interface

--- src/@types/api.d.ts
+++ src/@types/api.d.ts
@@ -34,7 +34,28 @@ export interface GetArticlesResponse extends ErrorResponse {
 }
 
 /*
- * User
+ * 記事投稿
+ */
+export interface CreateArticleRequest {
+  title: string
+  content: string
+  imageUrl?: string
+  contentCount: number
+  userId: string
+}
+export interface CreateArticleResponse extends ErrorResponse {
+  id: string
+}
+
+/*
+ * 記事更新
+ */
+export interface UpdateArticleRequest extends Partial<CreateArticleRequest> {
+  id: string
+}
+export interface UpdateArticleResponse extends ErrorResponse {
+  id: string
+}
  */
 export interface GetUsersRequest extends Partial<MicroCmsRequest> {}
 export interface GetUsersResponse extends ErrorResponse {
@@ -42,4 +63,29 @@ export interface GetUsersResponse extends ErrorResponse {
   totalCount: number
   offset: number
   limit: number
+}
+
+/*
+ * ユーザー作成
+ */
+export interface CreateUserRequest {
+  name: string
+  photoUrl: string
+  description?: string
+  twitterUrl?: string
+  facebookUrl?: string
+  userId: string
+}
+export interface CreateUserResponse extends ErrorResponse {
+  id: string
+}
+
+/*
+ * ユーザー更新
+ */
+export interface UpdateUserRequest extends Partial<CreateUserRequest> {
+  id: string
+}
+export interface UpdateUserResponse extends ErrorResponse {
+  id: string
 }
