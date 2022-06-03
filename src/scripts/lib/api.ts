258f17edb3b7c8332047855774acaa92f5c7f017
kira-0521 <kiratanaka0521@gmail.com>
2022-06-03 15:24:23 +0900

    [ feature ] API Fetch Articles

--- src/scripts/lib/api.ts
+++ src/scripts/lib/api.ts
@@ -1,6 +1,42 @@
-import apiInstance from './axios'
+import { isNil } from 'lodash'
 
-export const test = async () => {
-  const res = await apiInstance.get('articles')
-  return res
+import {
+  MicroCmsResponse,
+  GetArticlesResponse,
+  GetArticlesRequest,
+} from '../../@types/api'
+import { Article } from '../../@types/article'
+import apiInstance from './axios'
+import { ERROR_CODES } from './error'
+import { errorHandler } from './responseErrorHandler'
+
+/*
+ * GET 記事取得
+ */
+export const fetchArticles = async (
+  params?: GetArticlesRequest
+): Promise<GetArticlesResponse> => {
+  try {
+    const res = await apiInstance.get<MicroCmsResponse<Article>>('articles', {
+      params,
+    })
+    const validated = errorHandler(res)
+    if (!isNil(validated)) {
+      return validated
+    }
+    return {
+      ...res.data,
+      errCode: ERROR_CODES.NORMAL_NOOP.errCode,
+      errMsg: ERROR_CODES.NORMAL_NOOP.errMsg,
+    }
+  } catch (e) {
+    return {
+      contents: [],
+      totalCount: 0,
+      offset: 0,
+      limit: 0,
+      errCode: ERROR_CODES.INTERNAL_SERVER_ERROR.errCode,
+      errMsg: ERROR_CODES.INTERNAL_SERVER_ERROR.errMsg,
+    }
+  }
 }
