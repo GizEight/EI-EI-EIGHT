9713153c6f53c80ea9f950f372e2753511b6ea1f
kira-0521 <kiratanaka0521@gmail.com>
2022-06-03 15:32:58 +0900

    [ feature ] API Fetch Users

--- src/scripts/lib/api.ts
+++ src/scripts/lib/api.ts
@@ -4,8 +4,11 @@ import {
   MicroCmsResponse,
   GetArticlesResponse,
   GetArticlesRequest,
+  GetUsersRequest,
+  GetUsersResponse,
 } from '../../@types/api'
 import { Article } from '../../@types/article'
+import { User } from '../../@types/user.d'
 import apiInstance from './axios'
 import { ERROR_CODES } from './error'
 import { errorHandler } from './responseErrorHandler'
@@ -45,13 +48,27 @@ export const fetchArticles = async (
     return CATCH_RESPONSE
   }
 }
-    return {
-      contents: [],
-      totalCount: 0,
-      offset: 0,
-      limit: 0,
-      errCode: ERROR_CODES.INTERNAL_SERVER_ERROR.errCode,
-      errMsg: ERROR_CODES.INTERNAL_SERVER_ERROR.errMsg,
+
+/*
+GET ユーザー
+*/
+export const fetchUsers = async (
+  params?: GetUsersRequest
+): Promise<GetUsersResponse> => {
+  try {
+    const res = await apiInstance.get<MicroCmsResponse<User>>('users', {
+      params,
+    })
+    const validated = errorHandler(res)
+    if (!isNil(validated)) {
+      return validated
     }
+    return {
+      ...res.data,
+      errCode: ERROR_CODES.NORMAL_NOOP.errCode,
+      errMsg: ERROR_CODES.NORMAL_NOOP.errMsg,
+    }
+  } catch (e) {
+    return CATCH_RESPONSE
   }
 }
