46751639b8147fb8ecd1946e3c0b4aae190a4019
kira-0521 <kiratanaka0521@gmail.com>
2022-06-04 07:48:10 +0900

    [ feature ] ログイン状態の監視をAppで行う

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,67 @@
+import { onAuthStateChanged, User as FirebaseUser } from 'firebase/auth'
+import { isNil, isEmpty } from 'lodash'
 import { FC, useEffect } from 'react'
 
+import { useAppDispatch } from './app/hooks'
+import { login, logout } from './app/slices/userSlice'
 import { AuthTest } from './components/test/AuthTest'
-import { fetchUsers } from './scripts/lib/api'
+import { auth } from './firebase'
+import { fetchUsers, createUser } from './scripts/lib/api'
 
 const App: FC = () => {
-  // * test
+  const dispatch = useAppDispatch()
+  /*
+  ? ログイン状況監視
+  */
   useEffect(() => {
-    fetchUsers({ limit: 1 }).then((res) => {
-      // eslint-disable-next-line no-console
-      console.log('res', res)
-    })
+    const unSubscribe = () => {
+      onAuthStateChanged(auth, (user: FirebaseUser | null) => {
+        if (isNil(user)) {
+          dispatch(logout())
+          return
+        }
+        /*
+        ? ユーザーが登録済か確認する
+        */
+        fetchUsers({
+          filters: `userId[equals]${user.uid}`,
+        }).then(async ({ contents }) => {
+          if (isEmpty(contents)) {
+            await createUser({
+              name: user.displayName || user.uid,
+              photoUrl: user.photoURL || '',
+              description: '',
+              twitterUrl: '',
+              facebookUrl: '',
+              userId: user.uid,
+            })
+            dispatch(
+              login({
+                name: user.displayName || user.uid,
+                photoURL: user.photoURL || '',
+                description: '',
+                twitterUrl: '',
+                facebookUrl: '',
+                userId: user.uid,
+              })
+            )
+          } else {
+            dispatch(
+              login({
+                name: contents[0].name || contents[0].userId,
+                photoURL: contents[0].photoURL || '',
+                description: contents[0].description || '',
+                twitterUrl: contents[0].twitterUrl || '',
+                facebookUrl: contents[0].facebookUrl || '',
+                userId: contents[0].userId,
+              })
+            )
+          }
+          unSubscribe()
+        })
+      })
+    }
+    return () => unSubscribe()
   }, [])
 
   return (
--- src/components/test/AuthTest.tsx
+++ src/components/test/AuthTest.tsx
@@ -1,9 +1,7 @@
-import { isEmpty } from 'lodash'
 import { FC, useCallback } from 'react'
 
 import { useAppDispatch } from '../../app/hooks'
 import { setToast } from '../../app/slices/toastSlice'
-import { fetchUsers, createUser } from '../../scripts/lib/api'
 import { ERROR_CODES } from '../../scripts/lib/error'
 import { authGoogleLogin } from '../../scripts/lib/firebase/auth'
 
@@ -20,24 +18,6 @@ export const AuthTest: FC = () => {
           message: `code: ${res.errCode} - ${res.errMsg}`,
         })
       )
-      return
-    }
-    /*
-    ? ユーザーが登録済か確認する
-    */
-    const { contents } = await fetchUsers({
-      filters: `userId[equals]${res.user.uid}`,
-    })
-    if (isEmpty(contents)) {
-      const { user } = res
-      await createUser({
-        name: user.displayName || user.uid,
-        photoUrl: user.photoURL || '',
-        description: '',
-        twitterUrl: '',
-        facebookUrl: '',
-        userId: user.uid,
-      })
     }
   }, [])
 
