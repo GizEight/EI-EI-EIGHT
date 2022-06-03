b140c3ec07f1c31fd5f2c78172d8cdbead648fa5
kira-0521 <kiratanaka0521@gmail.com>
2022-06-03 19:42:47 +0900

    [ feature test ] checked register user on cms after login is success

--- src/components/test/AuthTest.tsx
+++ src/components/test/AuthTest.tsx
@@ -1,12 +1,44 @@
+import { isEmpty } from 'lodash'
 import { FC, useCallback } from 'react'
 
+import { useAppDispatch } from '../../app/hooks'
+import { setToast } from '../../app/slices/toastSlice'
+import { fetchUsers, createUser } from '../../scripts/lib/api'
+import { ERROR_CODES } from '../../scripts/lib/error'
 import { authGoogleLogin } from '../../scripts/lib/firebase/auth'
 
 export const AuthTest: FC = () => {
+  const dispatch = useAppDispatch()
+
   const onClickGoogleLogin = useCallback(async () => {
     const res = await authGoogleLogin()
-    // eslint-disable-next-line no-console
-    console.log('res', res)
+    if (res.errCode !== ERROR_CODES.NORMAL_NOOP.errCode) {
+      dispatch(
+        setToast({
+          isShow: true,
+          type: 'error',
+          message: `code: ${res.errCode} - ${res.errMsg}`,
+        })
+      )
+      return
+    }
+    /*
+    ? ユーザーが登録済か確認する
+    */
+    const { contents } = await fetchUsers({
+      filters: `userId[equals]${res.user.uid}`,
+    })
+    if (isEmpty(contents)) {
+      const { user } = res
+      await createUser({
+        name: user.displayName || user.uid,
+        photoUrl: user.photoURL || '',
+        description: '',
+        twitterUrl: '',
+        facebookUrl: '',
+        userId: user.uid,
+      })
+    }
   }, [])
 
   return (
