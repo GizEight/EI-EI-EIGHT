23a289ef83c1b5a85d564d77212d91f9c987ba03
kira-0521 <kiratanaka0521@gmail.com>
2022-06-03 18:12:36 +0900

    [ add ] 投稿・作成・更新用API

--- src/scripts/lib/api.ts
+++ src/scripts/lib/api.ts
@@ -1,11 +1,22 @@
-import { isNil } from 'lodash'
+import { isNil, omit } from 'lodash'
 
 import {
   MicroCmsResponse,
   GetArticlesResponse,
+  GetMicroCmsResponse,
   GetArticlesRequest,
+  ExceptingGetMicroCmsResponse,
+  CreateArticleRequest,
+  UpdateArticleRequest,
+  GetArticlesResponse,
+  CreateArticleResponse,
+  UpdateArticleResponse,
   GetUsersRequest,
+  CreateUserRequest,
+  UpdateUserRequest,
   GetUsersResponse,
+  CreateUserResponse,
+  UpdateUserResponse,
 } from '../../@types/api'
 import { Article } from '../../@types/article'
 import { User } from '../../@types/user.d'
@@ -21,6 +32,11 @@ const CATCH_RESPONSE = {
   totalCount: 0,
   offset: 0,
   limit: 0,
+  errCode: ERROR_CODES.INTERNAL_SERVER_ERROR.errCode,
+  errMsg: ERROR_CODES.INTERNAL_SERVER_ERROR.errMsg,
+}
+const CATCH_RESPONSE_EXCEPTING_GET = {
+  id: '',
   errCode: ERROR_CODES.INTERNAL_SERVER_ERROR.errCode,
   errMsg: ERROR_CODES.INTERNAL_SERVER_ERROR.errMsg,
 }
@@ -52,6 +68,47 @@ export const fetchArticles = async (
 /*
 GET ユーザー
 */
+ * 記事投稿
+ */
+export const postArticle = async (
+  params: CreateArticleRequest
+): Promise<CreateArticleResponse> => {
+  try {
+    const res = await apiInstance.post<ExceptingGetMicroCmsResponse>(
+      'articles',
+      params
+    )
+    return {
+      ...res.data,
+      errCode: ERROR_CODES.NORMAL_NOOP.errCode,
+      errMsg: ERROR_CODES.NORMAL_NOOP.errMsg,
+    }
+  } catch (e) {
+    return CATCH_RESPONSE_EXCEPTING_GET
+  }
+}
+
+/*
+ * 記事更新
+ */
+export const updateArticle = async (
+  params: UpdateArticleRequest
+): Promise<UpdateArticleResponse> => {
+  const requestParams: Omit<UpdateArticleRequest, 'id'> = omit(params, 'id')
+  try {
+    const res = await apiInstance.post<ExceptingGetMicroCmsResponse>(
+      `articles/${params.id}`,
+      requestParams
+    )
+    return {
+      ...res.data,
+      errCode: ERROR_CODES.NORMAL_NOOP.errCode,
+      errMsg: ERROR_CODES.NORMAL_NOOP.errMsg,
+    }
+  } catch (e) {
+    return CATCH_RESPONSE_EXCEPTING_GET
+  }
+}
 export const fetchUsers = async (
   params?: GetUsersRequest
 ): Promise<GetUsersResponse> => {
@@ -70,5 +127,45 @@ export const fetchUsers = async (
     }
   } catch (e) {
     return CATCH_RESPONSE
+/*
+ * ユーザー作成
+ */
+export const createUser = async (
+  params: CreateUserRequest
+): Promise<CreateUserResponse> => {
+  try {
+    const res = await apiInstance.post<ExceptingGetMicroCmsResponse>(
+      'users',
+      params
+    )
+    return {
+      ...res.data,
+      errCode: ERROR_CODES.NORMAL_NOOP.errCode,
+      errMsg: ERROR_CODES.NORMAL_NOOP.errMsg,
+    }
+  } catch (e) {
+    return CATCH_RESPONSE_EXCEPTING_GET
+  }
+}
+
+/*
+ * 記事更新
+ */
+export const updateUser = async (
+  params: UpdateUserRequest
+): Promise<UpdateUserResponse> => {
+  const requestParams: Omit<UpdateUserRequest, 'id'> = omit(params, 'id')
+  try {
+    const res = await apiInstance.post<ExceptingGetMicroCmsResponse>(
+      `users/${params.id}`,
+      requestParams
+    )
+    return {
+      ...res.data,
+      errCode: ERROR_CODES.NORMAL_NOOP.errCode,
+      errMsg: ERROR_CODES.NORMAL_NOOP.errMsg,
+    }
+  } catch (e) {
+    return CATCH_RESPONSE_EXCEPTING_GET
   }
 }
