e420cd1300d02189b322cec20f75a86af8c0a59e
kira-0521 <kiratanaka0521@gmail.com>
2022-06-04 17:57:57 +0900

    [ feature ] fontawesome setting

---
+++ src/fontawesome.ts
@@ -0,0 +1,8 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faCircleCheck,
+  faCircleExclamation,
+  faCircleInfo,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCircleCheck, faCircleExclamation, faCircleInfo)
