e3576570d6b0cc4867659c5bb4a360b6e9ffdc07
kira-0521 <kiratanaka0521@gmail.com>
2022-06-04 09:53:11 +0900

    [ refactor ]

--- src/components/test/AuthTest.tsx
+++ src/components/test/AuthTest.tsx
@@ -14,6 +14,7 @@ export const AuthTest: FC = () => {
     ;<div>まだ表示できないよ...</div>
   }
 
+  // eslint-disable-next-line no-console
   console.log('data', data)
 
   const onClickGoogleLogin = useCallback(async () => {
