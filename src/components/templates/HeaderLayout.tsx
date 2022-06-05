d5f3777b9e406c52a21af463f699a45b55797a2e
kira-0521 <kiratanaka0521@gmail.com>
2022-06-05 14:17:07 +0900

    [ add ] navigate to top link

--- src/components/templates/HeaderLayout.tsx
+++ src/components/templates/HeaderLayout.tsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { isEmpty } from 'lodash'
 import { Suspense } from 'react'
+import { Link as RouterLink } from 'react-router-dom'
 
 import { useAppSelector } from '../../app/hooks'
 import { selectUser } from '../../app/slices/userSlice'
@@ -15,10 +16,12 @@ export const HeaderLayout = () => {
     <header className="l-header">
       <div className="l-header_inner">
         <div className="l-header_content">
-          <h1 className="l-header_content-title">
-            <FontAwesomeIcon icon={['fas', 'lightbulb']} />
-            <span>{APP_TITLE}</span>
-          </h1>
+          <RouterLink to="/">
+            <h1 className="l-header_content-title">
+              <FontAwesomeIcon icon={['fas', 'lightbulb']} />
+              <span>{APP_TITLE}</span>
+            </h1>
+          </RouterLink>
           <nav className="l-header_content-menu">
             {isEmpty(loginUser.user.userId) ? (
               // TODO: login: モーダル開く addnew: 記事作成ページ遷移 avatar: menu表示
