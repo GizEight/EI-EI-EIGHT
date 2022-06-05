fc4b531377485fda839d7b7bc60a938f344802e8
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2022-06-05 13:30:32 +0900

    [ fix] クラス名の修正

--- src/components/templates/FooterLayout.tsx
+++ src/components/templates/FooterLayout.tsx
@@ -4,15 +4,15 @@ import { APP_TITLE } from '../../scripts/utils/const'
 
 export const FooterLayout = () => (
   // TODO: フッターレイアウト
-  <footer className="footer">
-    <div className="footer_inner">
-      <div className="footer_title-with-icon">
+  <footer className="l-footer">
+    <div className="l-footer_inner">
+      <div className="l-footer_title-with-icon">
         {/* TODO: アイコンの表示 */}
         <FontAwesomeIcon icon={['fas', 'lightbulb']} />
         <span>{APP_TITLE}</span>
       </div>
-      <span className="footer_horizon" />
-      <div className="footer_friends">
+      <span className="l-footer_horizon" />
+      <div className="l-footer_friends">
         <p>Kira tanaka</p>
         <p>Kazumaru Kato</p>
         <p>Seiya Iwanabe</p>
