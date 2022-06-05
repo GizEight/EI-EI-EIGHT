02b691a6c478e6431d0e1e5f01327b79cd8c8a30
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2022-06-05 13:23:38 +0900

    [ feature ] styling-footer

--- src/components/templates/FooterLayout.tsx
+++ src/components/templates/FooterLayout.tsx
@@ -1,4 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { APP_TITLE } from '../../scripts/utils/const'
+
 export const FooterLayout = () => (
   // TODO: フッターレイアウト
-  <footer>フッター</footer>
+  <footer className="footer">
+    <div className="footer_inner">
+      <div className="footer_title-with-icon">
+        {/* TODO: アイコンの表示 */}
+        <FontAwesomeIcon icon={['fas', 'lightbulb']} />
+        <span>{APP_TITLE}</span>
+      </div>
+      <span className="footer_horizon" />
+      <div className="footer_friends">
+        <p>Kira tanaka</p>
+        <p>Kazumaru Kato</p>
+        <p>Seiya Iwanabe</p>
+      </div>
+    </div>
+  </footer>
 )
