74ce83bb2209a6fb94e592740b2944b69c75b0fa
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2022-06-05 13:47:20 +0900

    [ add ] プロジェクトメンバーの配列を追加

--- src/components/templates/FooterLayout.tsx
+++ src/components/templates/FooterLayout.tsx
@@ -2,21 +2,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import { APP_TITLE } from '../../scripts/utils/const'
 
-export const FooterLayout = () => (
-  // TODO: フッターレイアウト
-  <footer className="l-footer">
-    <div className="l-footer_inner">
-      <div className="l-footer_title-with-icon">
-        {/* TODO: アイコンの表示 */}
-        <FontAwesomeIcon icon={['fas', 'lightbulb']} />
-        <span>{APP_TITLE}</span>
+export const FooterLayout = () => {
+  const members = ['Kira Tanaka', 'Kazumaru Katou', 'Seiya Iwanabe']
+
+  return (
+    <footer className="l-footer">
+      <div className="l-footer_inner">
+        <div className="l-footer_title-with-icon">
+          {/* TODO: アイコンの表示 */}
+          <FontAwesomeIcon icon={['fas', 'lightbulb']} />
+          <span>{APP_TITLE}</span>
+        </div>
+        <span className="l-footer_horizon" />
+        <div className="l-footer_friends">
+          {members.map((member, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <p key={index}>{member}</p>
+          ))}
+        </div>
       </div>
-      <span className="l-footer_horizon" />
-      <div className="l-footer_friends">
-        <p>Kira tanaka</p>
-        <p>Kazumaru Kato</p>
-        <p>Seiya Iwanabe</p>
-      </div>
-    </div>
-  </footer>
-)
+    </footer>
+  )
+}
