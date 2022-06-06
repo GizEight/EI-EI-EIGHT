e5755898652cf7c199340d1017b27e9570b73d80
kira-0521 <kiratanaka0521@gmail.com>
2022-06-06 12:59:40 +0900

    [ fix ] fetch時の引数指定

--- src/scripts/hooks/useQueryUsers.ts
+++ src/scripts/hooks/useQueryUsers.ts
@@ -1,12 +1,20 @@
+import { isNil } from 'lodash'
 import { useQuery } from 'react-query'
 
 import { GetUsersResponse } from '../../@types/api.d'
 import { fetchUsers } from '../lib/api'
 import { USE_QUERY_STALE_TIME, CACHE_KEY_USER } from '../utils/const'
 
-export const useQueryUsers = () =>
+type Props = {
+  userId: string
+}
+
+export const useQueryUsers = (props?: Props) =>
   useQuery<GetUsersResponse>({
     queryKey: CACHE_KEY_USER,
-    queryFn: (context) => fetchUsers(context.pageParam),
+    queryFn: () =>
+      fetchUsers(
+        isNil(props) ? undefined : { filters: `userId[equals]${props.userId}` }
+      ),
     staleTime: USE_QUERY_STALE_TIME,
   })
