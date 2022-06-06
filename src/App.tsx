abf118fed2194bf38f78a93da272ff1999d4911d
kira-0521 <kiratanaka0521@gmail.com>
2022-06-06 11:49:00 +0900

    [ add ] login時のmutate

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,19 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { useAppDispatch } from './app/hooks'
-import { login, logout } from './app/slices/userSlice'
+import { logout } from './app/slices/userSlice'
 import { Toast } from './components/atoms/Toast'
 import { LayoutsWrapper } from './components/templates/LayoutsWrapper'
 import { auth } from './firebase'
 import { HOME_ROUTES } from './routes'
+import { useMutateUsers } from './scripts/hooks/useMutateUsers'
 import { useToast } from './scripts/hooks/useToast'
-import { fetchUsers, createUser } from './scripts/lib/api'
-
+import { fetchUsers } from './scripts/lib/api'
 
 const App: FC = () => {
   const dispatch = useAppDispatch()
   const { toast, resetToast, loginSuccessToast, onClickCloseToast } = useToast()
+  const { registerUserCache, createUserMutation } = useMutateUsers()
 
   /*
   ? ログイン状況監視
@@ -36,8 +37,8 @@ const App: FC = () => {
         */
         fetchUsers({
           filters: `userId[equals]${user.uid}`,
-        }).then(async ({ contents }) => {
-          if (isEmpty(contents)) {
+        }).then(async (res) => {
+          if (isEmpty(res.contents)) {
             const signInNewUser = {
               name: user.displayName || user.uid,
               photoURL: user.photoURL || '',
@@ -46,19 +47,9 @@ const App: FC = () => {
               facebookUrl: '',
               userId: user.uid,
             }
-            await createUser(signInNewUser)
-            dispatch(login(signInNewUser))
+            createUserMutation.mutate(signInNewUser)
           } else {
-            dispatch(
-              login({
-                name: contents[0].name || contents[0].userId,
-                photoURL: contents[0].photoURL || '',
-                description: contents[0].description || '',
-                twitterUrl: contents[0].twitterUrl || '',
-                facebookUrl: contents[0].facebookUrl || '',
-                userId: contents[0].userId,
-              })
-            )
+            registerUserCache(res)
           }
         })
       }
