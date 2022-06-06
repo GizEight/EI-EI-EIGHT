6442bf535cbc38edb83a95a83f5c30afe5c8d066
kira-0521 <kiratanaka0521@gmail.com>
2022-06-06 11:23:03 +0900

    [ fix ] articles cache

--- src/scripts/hooks/useMutateArticles.ts
+++ src/scripts/hooks/useMutateArticles.ts
@@ -2,8 +2,7 @@
 import { isNil, map } from 'lodash'
 import { useQueryClient, useMutation } from 'react-query'
 
-import { GetMicroCmsResponse } from '../../@types/api.d'
-import { ResponseArticle } from '../../@types/article'
+import { GetArticlesResponse } from '../../@types/api.d'
 import { postArticle, updateArticle, fetchArticles } from '../lib/api'
 import { CACHE_KEY_ARTICLE } from '../utils/const'
 
@@ -13,15 +12,10 @@ export const useMutateArticles = () => {
   const createArticleMutation = useMutation(postArticle, {
     onSuccess: () => {
       const previousArticles =
-        queryClient.getQueryData<GetMicroCmsResponse<ResponseArticle>>(
-          CACHE_KEY_ARTICLE
-        )
+        queryClient.getQueryData<GetArticlesResponse>(CACHE_KEY_ARTICLE)
       if (!isNil(previousArticles)) {
         fetchArticles().then((res) => {
-          queryClient.setQueryData<GetMicroCmsResponse<ResponseArticle>>(
-            CACHE_KEY_ARTICLE,
-            res
-          )
+          queryClient.setQueryData<GetArticlesResponse>(CACHE_KEY_ARTICLE, res)
         })
       }
     },
@@ -30,20 +24,15 @@ export const useMutateArticles = () => {
   const updateArticleMutation = useMutation(updateArticle, {
     onSuccess: (data) => {
       const previousArticles =
-        queryClient.getQueryData<GetMicroCmsResponse<ResponseArticle>>(
-          CACHE_KEY_ARTICLE
-        )
+        queryClient.getQueryData<GetArticlesResponse>(CACHE_KEY_ARTICLE)
       if (!isNil(previousArticles)) {
         fetchArticles({ filters: `id[equals]${data.id}` }).then((res) => {
-          queryClient.setQueryData<GetMicroCmsResponse<ResponseArticle>>(
-            CACHE_KEY_ARTICLE,
-            {
-              ...previousArticles,
-              contents: map(previousArticles.contents, (article) =>
-                article.id === res.contents[0].id ? res.contents[0] : article
-              ),
-            }
-          )
+          queryClient.setQueryData<GetArticlesResponse>(CACHE_KEY_ARTICLE, {
+            ...previousArticles,
+            contents: map(previousArticles.contents, (article) =>
+              article.id === res.contents[0].id ? res.contents[0] : article
+            ),
+          })
         })
       }
     },
