23d1684e2486ca8e473f487e3ff2af1e4f3aa779
kira-0521 <kiratanaka0521@gmail.com>
2022-06-08 10:48:14 +0900

    [ add ] calc article created formatter

--- src/scripts/utils/dateFormat.ts
+++ src/scripts/utils/dateFormat.ts
@@ -15,3 +15,21 @@ export const nowDate = () => dayjs().format(NOW_FORMAT)
  * Date Formatter
  */
 export const formatDate = (date: string) => dayjs(date).format(DAY_FORMAT)
+
+/*
+ * ~日前 ~時間前
+ */
+export const calculateDate = (date: string) => {
+  const calc = dayjs().valueOf() - dayjs(date).valueOf()
+
+  // 日単位の差分
+  const diff = calc / (60 * 60 * 24 * 1000)
+
+  // 1日未満
+  if (diff < 1) {
+    return `${Math.ceil(calc / 1000000)}時間前`
+  }
+
+  // 1日以上
+  return `${Math.floor(diff)}日前`
+}
