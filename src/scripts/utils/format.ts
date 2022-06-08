db5d9b162fc516486fae02bad1f3a1372cddb998
kira-0521 <kiratanaka0521@gmail.com>
2022-06-08 09:53:25 +0900

    [ add ] comment

--- src/scripts/utils/format.ts
+++ src/scripts/utils/format.ts
@@ -1,8 +1,17 @@
 import dayjs from 'dayjs'
 
+/*
+ * FORMAT
+ */
 const NOW_FORMAT = 'YYYY/MM/DD HH:mm:ss'
 const DAY_FORMAT = 'YYYY/MM/DD'
 
+/*
+ * NOW
+ */
 export const nowDate = () => dayjs().format(NOW_FORMAT)
 
+/*
+ * Date Formatter
+ */
 export const formatDate = (date: string) => dayjs(date).format(DAY_FORMAT)
--- src/scripts/utils/logger.ts
+++ src/scripts/utils/logger.ts
@@ -1,5 +1,8 @@
 import { nowDate } from './format'
 
+/*
+ * Add Now Time Logger
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const debugClientLog = (first?: any, second?: any) => {
   // eslint-disable-next-line no-console
