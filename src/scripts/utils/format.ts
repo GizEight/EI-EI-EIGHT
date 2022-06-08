5cc91088fb39631d8bb56ac65de26790277def9c
kira-0521 <kiratanaka0521@gmail.com>
2022-06-08 09:47:15 +0900

    [ add ] nowDate util

--- src/scripts/utils/format.ts
+++ src/scripts/utils/format.ts
@@ -1,5 +1,8 @@
 import dayjs from 'dayjs'
 
+const NOW_FORMAT = 'YYYY/MM/DD HH:mm:ss'
 const DAY_FORMAT = 'YYYY/MM/DD'
+
+export const nowDate = () => dayjs().format(NOW_FORMAT)
 
 export const formatDate = (date: string) => dayjs(date).format(DAY_FORMAT)
