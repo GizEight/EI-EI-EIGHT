94ff7927db045658b4a7908b537dd4ff9b444c9f
kira-0521 <kiratanaka0521@gmail.com>
2022-06-08 13:27:15 +0900

    [ refactor ] delete cms default

--- src/@types/article.d.ts
+++ src/@types/article.d.ts
@@ -1,5 +1,3 @@
-import { CmsResponseDefault } from './cms.d'
-
 export interface Article {
   imageUrl: string
   title: string
@@ -8,4 +6,15 @@ export interface Article {
   contentCount: number
 }
 
-export interface ResponseArticle extends CmsResponseDefault, Article {}
+export interface ResponseArticle {
+  imageUrl: string
+  title: string
+  userId: string
+  content: string
+  contentCount: number
+  id: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+}
--- src/@types/cms.d.ts
+++
@@ -1,7 +0,0 @@
-export interface CmsResponseDefault {
-  id: string
-  createdAt: string
-  updatedAt: string
-  publishedAt: string
-  revisedAt: string
-}
