de0cd023ef1f0c2b90ad6c8022b352049f1ef808
kira-0521 <kiratanaka0521@gmail.com>
2022-06-13 18:40:49 +0900

    [ add ] form logic

--- src/components/organisms/SearchInput.tsx
+++ src/components/organisms/SearchInput.tsx
@@ -1,18 +1,39 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import { FC, memo } from 'react'
+import { useForm } from 'react-hook-form'
 
-export const SearchInput = () => (
-  <label className="c-form search-label" htmlFor="search">
-    <input type="text" className="c-form-search" id="search" />
-    <FontAwesomeIcon
-      size="lg"
-      icon={['fab', 'searchengin']}
-      className="c-form-search-icon"
-    />
-  </label>
-)
+import { ErrorMessage } from '../atoms/ErrorMessage'
+
+type Input = {
+  searchInput: string
+}
+
+export const SearchInput: FC = memo(() => {
+  const {
+    register,
+    formState: { errors },
+  } = useForm<Input>()
+
+  return (
+    <>
+      {errors.searchInput && (
+        <ErrorMessage
+          message={
+            errors.searchInput.message || '正しい形式で入力してください。'
+          }
+        />
+      )}
+      <label className="c-form search-label" htmlFor="search">
+        <input
+          className="c-form-search"
+          {...register('searchInput', { maxLength: 256 })}
+        />
         <FontAwesomeIcon
           size="lg"
           icon={['fas', 'magnifying-glass']}
           className="c-form-search-icon"
         />
+      </label>
+    </>
+  )
+})
