ffc881cce06457220d5471fac7fbd0d64133b329
kira-0521 <kiratanaka0521@gmail.com>
2022-06-14 08:07:20 +0900

    [ fix ] mainからsectionにかけての構成

--- src/components/templates/SectionLayout.tsx
+++ src/components/templates/SectionLayout.tsx
@@ -1,11 +1,15 @@
+import { isNil } from 'lodash'
 import { ReactNode, FC } from 'react'
 
 type Props = {
   children: ReactNode
+  sectionName?: string
 }
 
-export const SectionLayout: FC<Props> = ({ children }) => (
-  <section className="p-section">
+export const SectionLayout: FC<Props> = ({ children, sectionName }) => (
+  <section
+    className={`p-section ${!isNil(sectionName) && `p-section-${sectionName}`}`}
+  >
     <div className="p-section_inner">{children}</div>
   </section>
 )
