943e52a28880a6e3cf461fe377c9e2674e54db0b
kira-0521 <kiratanaka0521@gmail.com>
2022-06-14 15:34:43 +0900

    [ feature ] 記事の投稿・更新時コンテンツの表示管理

--- src/components/templates/HeaderLayout.tsx
+++ src/components/templates/HeaderLayout.tsx
@@ -1,10 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { isEmpty } from 'lodash'
-import { Suspense, useState, memo } from 'react'
+import { Suspense, useState, memo, useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { useAppSelector } from '../../app/hooks'
+import { selectArticle } from '../../app/slices/articleSlice'
 import { selectUser } from '../../app/slices/userSlice'
 import { useAuth } from '../../scripts/hooks/useAuth'
+import { useMutateArticles } from '../../scripts/hooks/useMutateArticles'
 import { useQueryUsers } from '../../scripts/hooks/useQueryUsers'
 import { APP_TITLE } from '../../scripts/utils/const'
 import { Popover } from '../atoms/Popover'
@@ -13,67 +16,83 @@ import { RouterLink } from '../atoms/RouterLink'
 import { Avatar } from '../molecules/Avatar'
 
 export const HeaderLayout = memo(() => {
-  // TODO: loading時にボタンにスピナー表示させる
+  /*
+   * Hooks
+   */
   const { login, logout } = useAuth()
+  const navigate = useNavigate()
   const { user } = useAppSelector(selectUser)
+  const { article } = useAppSelector(selectArticle)
+  const { data } = useQueryUsers({ userId: user.userId })
+  const { createArticleMutation, updateArticleMutation } = useMutateArticles()
 
-  const { data, status } = useQueryUsers({ userId: user.userId })
-
+  /*
+   * State
+   */
   const [isShowPop, setIsShowPop] = useState(false)
 
-  // TODO: エラー処理
-  if (status === 'error') {
-    return <div>Error</div>
-  }
+  /*
 
   return (
     <header className="l-header">
       <div className="l-header_inner">
         <div className="l-header_content">
-          <RouterLink to="/">
-            <h1 className="l-header_content-title">
-              <FontAwesomeIcon icon={['fas', 'lightbulb']} />
-              <span>{APP_TITLE}</span>
-            </h1>
-          </RouterLink>
-          <nav className="l-header_content-menu">
-            {isEmpty(user.userId) ? (
-              <PrimaryButton onClick={login} isRounded>
-                <p className="u-icon-btn">
-                  <FontAwesomeIcon icon={['fab', 'google']} />
-                  <span>Login with Google</span>
-                </p>
-              </PrimaryButton>
-            ) : (
-              <>
-                <Suspense fallback={<span>Loading...</span>}>
-                  <button onClick={() => setIsShowPop(!isShowPop)}>
-                    <Avatar
-                      src={data?.contents[0].photoURL || ''}
-                      alt={data?.contents[0].name}
-                    />
-                  </button>
-                  {isShowPop && (
-                    <Popover>
-                      <div className="c-popover_item">
-                        <PrimaryButton
-                          onClick={() => {
-                            logout()
-                            setIsShowPop(false)
-                          }}
-                        >
-                          Log out
-                        </PrimaryButton>
-                      </div>
-                    </Popover>
-                  )}
-                </Suspense>
-                <RouterLink to="/article/create" isBtn>
-                  Add new
-                </RouterLink>
-              </>
-            )}
-          </nav>
+          {article.isEdit ? (
+            <button onClick={() => navigate('-1')}>
+              <FontAwesomeIcon icon={['fas', 'circle-arrow-left']} />
+            </button>
+          ) : (
+            <RouterLink to="/">
+              <h1 className="l-header_content-title">
+                <FontAwesomeIcon icon={['fas', 'lightbulb']} />
+                <span>{APP_TITLE}</span>
+              </h1>
+            </RouterLink>
+          )}
+          {article.isEdit ? (
+            <PrimaryButton onClick={onClickPost} isRounded>
+              Post it !
+            </PrimaryButton>
+          ) : (
+            <nav className="l-header_content-menu">
+              {isEmpty(user.userId) ? (
+                <PrimaryButton onClick={login} isRounded>
+                  <p className="u-icon-btn">
+                    <FontAwesomeIcon icon={['fab', 'google']} />
+                    <span>Login with Google</span>
+                  </p>
+                </PrimaryButton>
+              ) : (
+                <>
+                  <Suspense fallback={<span>Loading...</span>}>
+                    <button onClick={() => setIsShowPop(!isShowPop)}>
+                      <Avatar
+                        src={data?.contents[0].photoURL || ''}
+                        alt={data?.contents[0].name}
+                      />
+                    </button>
+                    {isShowPop && (
+                      <Popover>
+                        <div className="c-popover_item">
+                          <PrimaryButton
+                            onClick={() => {
+                              logout()
+                              setIsShowPop(false)
+                            }}
+                          >
+                            Log out
+                          </PrimaryButton>
+                        </div>
+                      </Popover>
+                    )}
+                  </Suspense>
+                  <RouterLink to="/article/create" isBtn>
+                    Add new
+                  </RouterLink>
+                </>
+              )}
+            </nav>
+          )}
         </div>
       </div>
     </header>
