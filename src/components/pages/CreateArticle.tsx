f3ca4a7435c90b9bd52dcaa78e6351f4f63b7d6d
kira-0521 <kiratanaka0521@gmail.com>
2022-06-14 16:27:54 +0900

    [ add ] マウント時とアンマウント時にisEditフラグを切り替える

--- src/components/pages/CreateArticle.tsx
+++ src/components/pages/CreateArticle.tsx
@@ -1,1 +1,14 @@
-export const CreateArticle = () => <div>CreateArticle</div>
+import { useEffect } from 'react'
+import { useAppDispatch } from '../../app/hooks'
+import { toggleEdit } from '../../app/slices/articleSlice'
+export const CreateArticle = () => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(toggleEdit(true))
+
+    return () => {
+      dispatch(toggleEdit(false))
+    }
+  }, [])
+
