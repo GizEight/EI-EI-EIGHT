93ae02f70015bd34c7a4b0015ae210ecf137b629
kira-0521 <kiratanaka0521@gmail.com>
2022-06-15 14:53:30 +0900

    [ add ] form更新用ロジック

--- src/components/pages/CreateArticle.tsx
+++ src/components/pages/CreateArticle.tsx
@@ -1,37 +1,66 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { isNil } from 'lodash'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 
+import { Forms } from '../../@types/view'
 import { useAppDispatch } from '../../app/hooks'
-import { toggleEdit } from '../../app/slices/articleSlice'
+import {
+  toggleEdit,
+  setEditContent,
+  setEditTitle,
+} from '../../app/slices/articleSlice'
 import { Form } from '../molecules/Form'
 import { IconButton } from '../molecules/IconButton'
 import { SectionLayout } from '../templates/SectionLayout'
 
-type Forms = {
-  title: string
-  content: string
-}
-
 export const CreateArticle = () => {
   const dispatch = useAppDispatch()
-  const { register } = useForm<Forms>()
+  const { register, watch } = useForm<Forms>()
 
   useEffect(() => {
     dispatch(toggleEdit(true))
+    const subscription = watch((value, { name }) => {
+      if (!isNil(name)) {
+        switch (name) {
+          case 'title':
+            dispatch(setEditTitle(value.title || ''))
+            break
+
+          case 'content':
+            dispatch(setEditContent(value.content || ''))
+            break
+          default:
+            break
+        }
+      }
+    })
 
     return () => {
       dispatch(toggleEdit(false))
+      subscription.unsubscribe()
     }
-  }, [])
+  }, [watch])
 
   return (
     <SectionLayout sectionName="create-article">
       <div className="p-section_content">
         <div className="p-section_content_forms">
-          <Form type="text" placeholder="Title..." />
+          <Form
+            type="text"
+            placeholder="Title..."
+            register={register}
+            name="title"
+            required
+          />
           <div className="p-section_content_forms_contents">
-            <Form type="textarea" placeholder="write in Markdown..." />
+            <Form
+              type="textarea"
+              placeholder="write in Markdown..."
+              register={register}
+              name="content"
+              required
+            />
             <div className="p-section_content_forms-buttons">
               <div className="c-icon-btn-double">
                 <IconButton onClick={() => console.log}>
