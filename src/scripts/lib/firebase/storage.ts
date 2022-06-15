d2952a272f02ce2537ccd5a19cc2b50b477c27e6
kira-0521 <kiratanaka0521@gmail.com>
2022-06-15 19:08:26 +0900

    [ add ] catch

--- src/scripts/lib/firebase/storage.ts
+++ src/scripts/lib/firebase/storage.ts
@@ -1,6 +1,10 @@
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+import { toNumber } from 'lodash'
 
+import { GetImageUrlRequest, GetImageUrlResponse } from '../../../@types/api'
 import { storage } from '../../../firebase'
+import { ERROR_CODES } from '../error'
+import { isFirebaseError } from './error'
 
 /*
 
@@ -8,13 +12,35 @@ storageに保存 => 画像のurl取得
 
 */
 export const getImageUrl = async (
-  dirName: string,
-  fileName: string,
-  imageFile: File
-) => {
-  const path = `${dirName}/${fileName}`
-  const storageRef = ref(storage, path)
-  await uploadBytes(storageRef, imageFile)
-  const url = await getDownloadURL(ref(storage, path))
-  return url
+  params: GetImageUrlRequest
+): Promise<GetImageUrlResponse> => {
+  const { dirName, fileName, imageFile } = params
+  try {
+    const path = `${dirName}/${fileName}`
+    const storageRef = ref(storage, path)
+    await uploadBytes(storageRef, imageFile)
+    const url = await getDownloadURL(ref(storage, path))
+    return {
+      url,
+      errCode: ERROR_CODES.NORMAL_NOOP.errCode,
+      errMsg: ERROR_CODES.NORMAL_NOOP.errMsg,
+    }
+  } catch (error) {
+    if (
+      error instanceof Error &&
+      isFirebaseError(error) &&
+      error.code === 'auth/account-exists-with-different-credential'
+    ) {
+      return {
+        url: '',
+        errCode: toNumber(error.code),
+        errMsg: error.message,
+      }
+    }
+    return {
+      url: '',
+      errCode: ERROR_CODES.INTERNAL_SERVER_ERROR.errCode,
+      errMsg: ERROR_CODES.INTERNAL_SERVER_ERROR.errMsg,
+    }
+  }
 }
