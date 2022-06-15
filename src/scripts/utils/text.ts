f490481f7c39d0655ccb8c53bef837b59efae9ee
kira-0521 <kiratanaka0521@gmail.com>
2022-06-15 16:00:17 +0900

    [ add ] random char util

---
+++ src/scripts/utils/text.ts
@@ -0,0 +1,18 @@
+import { join, map } from 'lodash'
+
+/*
+
+ランダムな16文字の文字列を生成
+
+*/
+export const getUniqueChar = (): string => {
+  const S = 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ01234567789'
+  const N = 16
+  return join(
+    map(
+      Array.from(crypto.getRandomValues(new Uint32Array(N))),
+      (n) => S[n % S.length]
+    ),
+    ''
+  )
+}
