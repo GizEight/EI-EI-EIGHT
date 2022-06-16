de20a83302ebc90d005282d2c6b8bdf1362afb79
kira-0521 <kiratanaka0521@gmail.com>
2022-06-16 11:27:34 +0900

    [ refactor ] input component props

--- src/components/molecules/ImageInput.tsx
+++ src/components/molecules/ImageInput.tsx
@@ -1,16 +1,16 @@
 import { IconName, IconPrefix } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { FC, memo, ChangeEvent } from 'react'
+import { FC, memo } from 'react'
+
+type InputProps = JSX.IntrinsicElements['input']
 
 type Props = {
   icon: [IconPrefix, IconName]
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void
-  id: string
-}
+} & InputProps
 
-export const ImageInput: FC<Props> = memo(({ icon, onChange, id }: Props) => (
-  <label htmlFor={id} className="c-form-image">
-    <FontAwesomeIcon icon={icon} size="lg" />
-    <input type="file" accept="image/*" id={id} onChange={onChange} />
+export const ImageInput: FC<Props> = memo<Props>((props: Props) => (
+  <label htmlFor={props.id} className="c-form-image">
+    <FontAwesomeIcon icon={props.icon} size="lg" />
+    <input type="file" accept="image/*" {...props} />
   </label>
 ))
