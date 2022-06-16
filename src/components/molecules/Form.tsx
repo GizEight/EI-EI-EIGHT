884998d8fbb5919ce9fb25808931b2c7c87a0cfb
kira-0521 <kiratanaka0521@gmail.com>
2022-06-16 11:01:07 +0900

    [ fix ] fixed for wrappable form component.

--- src/components/molecules/Form.tsx
+++ src/components/molecules/Form.tsx
@@ -1,67 +1,25 @@
-import { isNil } from 'lodash'
-import { FC, memo, useCallback } from 'react'
-import { UseFormRegister, Path } from 'react-hook-form'
-import TextareaAutosize from 'react-textarea-autosize'
+import { isNil, isEmpty } from 'lodash'
+import { FC, memo, ReactNode } from 'react'
 
-import { Forms } from '../../@types/view.d'
+import { ErrorMessage } from '../atoms/ErrorMessage'
 
 type NullAble<T> = T | null
 
 type Props = {
-  name: Path<Forms>
-  required: boolean
   label?: NullAble<string>
-  placeholder?: NullAble<string>
-  type: 'text' | 'textarea'
-  register: UseFormRegister<Forms>
-  maxLength?: number
+  children: ReactNode
+  errorMsg?: string
 }
 
 export const Form: FC<Props> = memo((props: Props) => {
-  const {
-    label,
-    placeholder,
-    type,
-    register,
-    name,
-    required,
-    maxLength = 256,
-  } = props
+  const { label = '', children, errorMsg = '' } = props
 
-  const switchFormType = useCallback(() => {
-    switch (type) {
-      case 'textarea':
-        return (
-          <TextareaAutosize
-            minRows={3}
-            className="c-form-form_textarea"
-            placeholder={placeholder || ''}
-            {...register(name, { required, maxLength })}
-          />
-        )
-
-      default:
-        return (
-          <input
-            type="text"
-            className="c-form-form_input"
-            placeholder={placeholder || ''}
-            {...register(name, { required, maxLength })}
-          />
-        )
-    }
-  }, [type])
-
-  const switchForm = useCallback(
-    () => (
-      <div className="c-form">
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        {!isNil(label) && <label>{label}</label>}
-        {switchFormType()}
-      </div>
-    ),
-    [label, switchFormType]
+  return (
+    <div className="c-form">
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      {!isNil(label) && <label>{label}</label>}
+      {children}
+      {!isEmpty(errorMsg) && <ErrorMessage>{errorMsg}</ErrorMessage>}
+    </div>
   )
-
-  return <>{switchForm()}</>
 })
