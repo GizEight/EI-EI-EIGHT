488f2c333079b506b67b643e03154f5784769cf5
kira-0521 <kiratanaka0521@gmail.com>
2022-06-17 15:33:18 +0900

    [ add ] no contents message

--- src/components/organisms/PreviewMarkdown.tsx
+++ src/components/organisms/PreviewMarkdown.tsx
@@ -1,3 +1,4 @@
+import { isEmpty } from 'lodash'
 import { FC, memo } from 'react'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
@@ -10,9 +11,13 @@ export const PreviewMarkdown: FC<Props> = memo((props: Props) => {
   const { markdown } = props
   return (
     <div className="c-form-form_textarea">
-      <ReactMarkdown remarkPlugins={[remarkGfm]} unwrapDisallowed={false}>
-        {markdown}
-      </ReactMarkdown>
+      {isEmpty(markdown) ? (
+        <p className="c-form-form_textarea_message">コンテンツがありません。</p>
+      ) : (
+        <ReactMarkdown remarkPlugins={[remarkGfm]} unwrapDisallowed={false}>
+          {markdown}
+        </ReactMarkdown>
+      )}
     </div>
   )
 })
