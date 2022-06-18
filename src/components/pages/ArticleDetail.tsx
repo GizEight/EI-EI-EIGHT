c58952b073ebc67016c0f63e0427c9e0c0e7d271
kira-0521 <kiratanaka0521@gmail.com>
2022-06-18 12:37:30 +0900

    [ add ] Header for Article detail

--- src/components/pages/ArticleDetail.tsx
+++ src/components/pages/ArticleDetail.tsx
@@ -1,1 +1,12 @@
-export const ArticleDetail = () => <div>ArticleDetail</div>
+import { DetailHeader } from '../molecules/ArticleDetail/DetailHeader'
+import { SectionLayout } from '../template/SectionLayout'
+
+// eslint-disable-next-line arrow-body-style
+export const ArticleDetail = () => {
+  return (
+    <SectionLayout sectionName="article-detail">
+      <DetailHeader thumbSrc="" thumbAlt="" title="タイトル" />
+      記事詳細
+    </SectionLayout>
+  )
+}
