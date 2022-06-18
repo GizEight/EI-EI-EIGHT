d8e79f26384ff26b477af114c743f30aecffcce3
kira-0521 <kiratanaka0521@gmail.com>
2022-06-18 16:04:52 +0900

    [ add ] get detail deta

--- src/components/pages/ArticleDetail.tsx
+++ src/components/pages/ArticleDetail.tsx
@@ -1,9 +1,27 @@
+import { isNil } from 'lodash'
+import { useParams } from 'react-router-dom'
+
+import { useQueryArticles } from '../../scripts/hooks/useQueryArticles'
+import { useQueryUsers } from '../../scripts/hooks/useQueryUsers'
 import { DetailContentWrapper } from '../molecules/ArticleDetail/DetailContent'
 import { DetailHeader } from '../molecules/ArticleDetail/DetailHeader'
 import { SectionLayout } from '../template/SectionLayout'
 
 // eslint-disable-next-line arrow-body-style
 export const ArticleDetail = () => {
+  /*
+   * Hooks
+   */
+  const params = useParams<{ id: string }>()
+  const { data: article, isLoading: articleIsLoading } = useQueryArticles({
+    filter: `id[equals]${params.id}`,
+  })
+  const { data: user, isLoading: userIsLoading } = useQueryUsers({
+    filter: isNil(article)
+      ? undefined
+      : `userId[equals]${article.contents[0].userId}`,
+  })
+
   return (
     <SectionLayout sectionName="article-detail">
       <DetailHeader thumbSrc="" thumbAlt="" title="タイトル" />
