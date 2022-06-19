8d28f9efc140529b36c6df3e58b7ca829b859ea4
kira-0521 <kiratanaka0521@gmail.com>
2022-06-19 21:22:29 +0900

    [ refactor ] rename

--- src/scripts/utils/format.ts
+++ src/scripts/utils/format.ts
@@ -3,7 +3,7 @@ import { size } from 'lodash'
 /*
  * 数字にコンマ付与
  */
-export const numberAddComma = (num: number): string => num.toLocaleString()
+export const addCommaForNumber = (num: number): string => num.toLocaleString()
 
 /*
  * 文字数カウント
@@ -14,5 +14,5 @@ export const stringCountFormatBy = (contents: string): string => {
   if (contentsCountResult < 100) {
     return `${size(contents)}文字`
   }
-  return `約${numberAddComma(Math.round(size(contents) / 100) * 100)}文字`
+  return `約${addCommaForNumber(Math.round(size(contents) / 100) * 100)}文字`
 }
