062238ce21bae5f01bc1c8fbe76be855dd3a0b8a
kira-0521 <kiratanaka0521@gmail.com>
2022-06-23 15:51:08 +0900

    [ fix ] wrapped form component

--- src/components/molecules/Form.tsx
+++ src/components/molecules/Form.tsx
@@ -5,20 +5,22 @@ import { FC, memo, ReactNode } from 'react'
 import { ErrorMessage } from '../atoms/ErrorMessage'
 
 type NullAble<T> = T | null
+type Undefinedable<T> = T | undefined
 
 type Props = {
   label?: NullAble<string>
+  id?: Undefinedable<string>
   children: ReactNode
   errorMsg?: string
 }
 
 export const Form: FC<Props> = memo((props: Props) => {
-  const { label = '', children, errorMsg = '' } = props
+  const { label = '', id = '', children, errorMsg = '' } = props
 
   return (
     <div className="c-form">
       {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-      {!isNil(label) && <label>{label}</label>}
+      {!isNil(label) && <label htmlFor={id}>{label}</label>}
       {children}
       <ErrorMessage className={clsx(isEmpty(errorMsg) && 'c-form-hidden')}>
         {errorMsg}
