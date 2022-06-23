f8408c0b897958594f96a8dda73329b3b05accc8
kira-0521 <kiratanaka0521@gmail.com>
2022-06-23 16:33:53 +0900

    [ add ] null support userdata

--- src/components/pages/EditUser.tsx
+++ src/components/pages/EditUser.tsx
@@ -23,40 +23,59 @@ export const EditUser = () => {
   return (
     <SectionLayout sectionName="edit-user">
       <SectionTitle>Settings</SectionTitle>
-      <div className="p-section_forms">
-        <Form id="username-form" label="お名前">
-          <Input
-            id="username-form"
-            placeholder="ユーザー名を入力してください。"
-            {...register('username', { maxLength: 256, required: true })}
-            isBg
-          />
-        </Form>
-        <Form id="description-form" label="自己紹介">
-          <Textarea
-            id="description-form"
-            placeholder="自己紹介を入力してください。"
-            {...register('description', { maxLength: 1000 })}
-            isBg
-          />
-        </Form>
-        <Form id="twitter-form" label="twitterリンク">
-          <Input
-            id="twitter-form"
-            placeholder="Twitter URLを入力してください。"
-            {...register('twitterUrl')}
-            isBg
-          />
-        </Form>
-        <Form id="instagram-form" label="instagramリンク">
-          <Input
-            id="instagram-form"
-            placeholder="ユーザー名を入力してください。"
-            {...register('instagramUrl', { maxLength: 256, required: true })}
-            isBg
-          />
-        </Form>
-      </div>
+      {isNil(userData) ? (
+        <div style={{ marginTop: '30px' }}>
+          <ErrorMessage>ユーザーが存在しません。</ErrorMessage>
+        </div>
+      ) : (
+        <form
+          className="p-section_forms"
+          onSubmit={handleSubmit((data) =>
+            updateUserMutation.mutate({
+              id: userData.contents[0].id,
+              username: data.username,
+              photoURL: '',
+              description: data.description,
+              twitterUrl: data.twitterUrl,
+              instagramUrl: data.instagramUrl,
+            })
+          )}
+        >
+          <Form id="username-form" label="お名前">
+            <Input
+              id="username-form"
+              placeholder="ユーザー名を入力してください。"
+              {...register('username', { maxLength: 256, required: true })}
+              isBg
+            />
+          </Form>
+          <Form id="description-form" label="自己紹介">
+            <Textarea
+              id="description-form"
+              placeholder="自己紹介を入力してください。"
+              {...register('description', { maxLength: 1000 })}
+              isBg
+            />
+          </Form>
+          <Form id="twitter-form" label="twitterリンク">
+            <Input
+              id="twitter-form"
+              placeholder="Twitter URLを入力してください。"
+              {...register('twitterUrl')}
+              isBg
+            />
+          </Form>
+          <Form id="instagram-form" label="instagramリンク">
+            <Input
+              id="instagram-form"
+              placeholder="ユーザー名を入力してください。"
+              {...register('instagramUrl', { maxLength: 256, required: true })}
+              isBg
+            />
+          </Form>
+          <PrimaryButton type="submit">Update</PrimaryButton>
+        </form>
+      )}
     </SectionLayout>
   )
 }
